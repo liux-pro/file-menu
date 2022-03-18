f2f65a6b0743c20f4aebad0d78d0672304fd1f20
Legend <me@liux.pro>
2022-03-18 18:26:47 +0800

    虚拟滚动

--- src/main/index.js
+++ src/main/index.js
@@ -28,13 +28,14 @@ function createWindow () {
   mainWindow.loadURL(winURL)
   // Open dev tools initially when in development mode
   if (process.env.NODE_ENV === 'development') {
-    mainWindow.webContents.on('did-frame-finish-load', () => {
-      mainWindow.webContents.once('devtools-opened', () => {
-        mainWindow.focus()
-      })
-      mainWindow.webContents.openDevTools()
-    })
+
   }
+  mainWindow.webContents.on('did-frame-finish-load', () => {
+    mainWindow.webContents.once('devtools-opened', () => {
+      mainWindow.focus()
+    })
+    mainWindow.webContents.openDevTools()
+  })
   mainWindow.on('closed', () => {
     mainWindow = null
   })
--- src/renderer/components/MainPage.vue
+++ src/renderer/components/MainPage.vue
@@ -10,7 +10,7 @@
                        :max="20"></el-input-number>
     </el-input>
     <el-button @click="exportExcel" slot="append">1</el-button>
-    <el-virtual-tree
+    <el-tree
         class="filter-tree"
         :data="data"
         :props="defaultProps"
@@ -19,9 +19,9 @@
         :expand-on-click-node="false"
         @node-click="handleNodeClick"
         :render-content="renderContent"
-        height="calc(100vh  - 80px)"
+        :height="400"
         ref="tree">
-    </el-virtual-tree>
+    </el-tree>
     <el-dialog
         :visible.sync="centerDialogVisible"
         width="60%"
@@ -46,7 +46,6 @@
 
 <script>
 import {getDirs, openDir, selectDir} from '../files'
-import ElVirtualTree from '/src/components/el-virtual-tree'
 
 export default {
   name: 'MainPage',
@@ -55,7 +54,6 @@ export default {
       this.$refs.tree.filter(val)
     }
   },
-  components: {ElVirtualTree},
   methods: {
     filterNode (value, data) {
       if (data.depth > this.maxDepth) return false
@@ -187,16 +185,15 @@ export default {
       this.centerDialogVisible = false
     },
     renderContent (h, {node, data, store}) {
-      console.log(data)
       const highlightLabel = this.highlight(data.label, this.filterText)
       const highlightComment = data.comment ? this.highlight(data.comment, this.filterText) : 'comment'
       return (
         <span class="tree">
-          <span class="red" domPropsInnerHTML={highlightLabel}></span>
-          <el-button type="success" plain size="large">
-            <i class="el-icon-s-comment"></i>
-            <span class="red" domPropsInnerHTML={highlightComment}></span>
-          </el-button>
+          <span class="red" domPropsInnerHTML={highlightLabel}/>
+          <button class="el-button el-button--default">
+            <i class="el-icon-s-comment"/>
+            <span class="red" domPropsInnerHTML={highlightComment}/>
+          </button>
         </span>)
     }
   },
